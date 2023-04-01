12c1df3e1fce3afc987b4cbcb8bcb1bba77b8939
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-04-01 12:07:05 +0300

    comparator done

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person (String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+    String getName () {
+        return name;
+    }
+    String getSurname () {
+        return surname;
+    }
+    int getAge () {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + " фамилия: " + surname + " возраст: " + age;
+    }
+}
