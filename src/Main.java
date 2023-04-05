57a27104cc640c849f2dc5b4662e25de4f5995d2
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-04-05 19:19:02 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +9,17 @@ public class Main {
         Person person1 = new Person("Nikita", "The Greatest Man", 17);
         Person person2 = new Person("Oleg", "Olegovich", 25);
         Person person3 = new Person("Jastin", "BI BI BI BER", 10);
+        Person person4 = new Person("Dima", "Dimkovich", 100);
         people.add(person1);
         people.add(person2);
         people.add(person3);
-        Collections.sort(people, comparator);
+        people.add(person4);
+        people.sort(comparator);
         System.out.println(people);
+        Predicate<Person> comparison;
+        comparison = (person) -> person.getAge() < 18;
+        people.removeIf(comparison);
+        System.out.println(people);
+
     }
 }
