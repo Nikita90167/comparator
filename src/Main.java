7acc770cbd69c7d81e8bc6a4e519f6bde87308cf
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-04-05 19:22:06 +0300

    done2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        PersonComparator comparator = new PersonComparator();
+        Comparator<Person> comparatorLambda;
+        comparatorLambda = (o1, o2) -> {
+            String[] countWordsPersonFirst = o1.getSurname().split("\\P{Alpha}+");
+            String[] countWordsPersonSecond = o2.getSurname().split("\\P{Alpha}+");
+            if (countWordsPersonFirst.length > countWordsPersonSecond.length) {
+                return 1;
+            } else if (countWordsPersonSecond.length > countWordsPersonFirst.length) {
+                return -1;
+            } else {
+                return o1.getAge() - o2.getAge();
+            }
+        };
+
         List<Person> people = new ArrayList<>();
         Person person1 = new Person("Nikita", "The Greatest Man", 17);
         Person person2 = new Person("Oleg", "Olegovich", 25);
@@ -14,11 +27,10 @@ public class Main {
         people.add(person2);
         people.add(person3);
         people.add(person4);
-        people.sort(comparator);
+        people.sort(comparatorLambda);
         System.out.println(people);
         Predicate<Person> comparison;
-        comparison = (person) -> person.getAge() < 18;
-        people.removeIf(comparison);
+        people.removeIf(comparison = (person) -> person.getAge() < 18);
         System.out.println(people);
 
     }
