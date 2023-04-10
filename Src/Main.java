14c38cbf85c1af7937589e71f37f3b2a1161bc59
Nikita90167 <124439125+Nikita90167@users.noreply.github.com>
2023-04-10 15:23:01 +0300

    Update Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
-
-
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
